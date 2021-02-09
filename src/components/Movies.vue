3e8a9d16ae4980468c5da967ea824db611f15957
01-mohit <hmohit05683@gmail.com>
2021-02-09 14:25:30 +0530

    some changes

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -23,29 +23,31 @@ export default {
             movies: [
                 {
                     id:1,
-                    name:'Avengers:Endgame',
-                    desc: 'Avengers: Endgame is a 2019 American superhero film based on the Marvel Comics superhero Avengers.',
+                    name:'Avengers:Endgame',                    
                     release: '26 April 2019',
                     img:'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg',
                     rating:'8.4'
                 },{
                     id:1,
-                    name:'Captain Marvel',
-                    desc: 'Captain Marvel is a 2019 American superhero film based on the Marvel Comics character Carol Danvers.',
+                    name:'Captain Marvel' ,                   
                     release: '08 March 2019',
                     img:'https://preview.redd.it/69lht2ghnke21.jpg?auto=webp&s=e6204664fb702b09dd4c522f6038796d9f52f1bd',
                     rating:'6.9'
                 },{
                     id:1,
-                    name:'Doctor Strange',
-                    desc: 'Doctor Strange is a 2016 American superhero film based on the Marvel Comics character of the same name.',
+                    name:'Infinity War',                    
+                    release: '23 April 2018',
+                    img:'https://wallpapercave.com/wp/wp3533778.jpg',
+                    rating:'8.4'
+                },{
+                    id:1,
+                    name:'Doctor Strange',                   
                     release: '4 Nov 2016',
                     img:'https://images-na.ssl-images-amazon.com/images/I/A12-NFRep6L._AC_SL1500_.jpg',
                     rating:'7.5'
                 },{
                     id:1,
-                    name:'Iron Man 3',
-                    desc: 'Iron Man 3 is a 2013 American superhero film based on the Marvel Comics character Iron Man.',
+                    name:'Iron Man 3',                   
                     release: '26 April 2013',
                     img:'https://i.pinimg.com/originals/cf/81/52/cf8152447ca6b83acbd3567cb2e05661.jpg',
                     rating:'7.1'
